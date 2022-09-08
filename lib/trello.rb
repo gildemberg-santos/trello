2547ca3aef509eebe242f1d9bfc24c0c466277ee
gildemberg-santos <gildemberg.santos@gmail.com>
2022-09-07 21:25:49 -0300

    Estrutura basica

--- lib/trello.rb
+++ lib/trello.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 require_relative "trello/version"
+require_relative "trello/error"
+require_relative "trello/base"
+require_relative "trello/debug"
+require_relative "trello/request"
 
 module Trello
-  class Error < StandardError; end
-  # Your code goes here...
 end
